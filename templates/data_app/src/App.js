637a296e2d9227060876a9a66e65b803667c0abe
shw97 <shwethasrinivasan97@gmail.com>
2019-09-21 22:34:21 +1000

    Update Changes

--- templates/data_app/src/App.js
+++ templates/data_app/src/App.js
@@ -7,7 +7,7 @@ import './App.css';
 import Api_retrieval from './components/api_retrieval';
 // import Maps_poly from './components/maps_poly';
  import Mapload from './components/mapload';
- import Gundb_demo from './components/gundb_demo'
+ // import Gundb_demo from './components/gundb_demo'
 
 class App extends React.Component {
   render() {
@@ -16,20 +16,18 @@ class App extends React.Component {
     <Router>
     <div className="App">
       <div>
-      	<ul className="topnav">
- 		 	// <li><a className="active" href="map.html">Home</a></li>
-  			<li><Link to={'/'}>Extra</Link></li>
-			<li className="right" ><Link to={'/Api_retrieval'}>Data Retrieval</Link></li>
+        <ul className="topnav">
+      // <li><a className="active" href="map.html">Home</a></li>
+        <li><Link to={'/'}>Extra</Link></li>
+      <li className="right" ><Link to={'/Api_retrieval'}>Data Retrieval</Link></li>
        <li className="right"><Link to={'/Mapload'}>Map Load</Link></li>
-       <li className="right"><Link to={'/Gundb_demo'}>Map Load</Link></li>
-		</ul>
+    </ul>
 
       </div>
     </div>
     <Switch>
         <Route path='/Api_retrieval' component={ Api_retrieval } />
         <Route path='/Mapload' component={ Mapload } />
-        <Route path='/Gundb_demo' component={ Gundb_demo } />
     </Switch>
     </Router>
     </div>
