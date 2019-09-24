d458c9832ad10ca8aee1836d88ff87103156b5e9
Shalitha Weerakoon Karunatilleke <shalithakaru@gmail.com>
2019-09-24 12:40:23 +1000

    [shalitha] minor fixes to the map

--- data-app/src/components/Map/Mapload.jsx
+++ data-app/src/components/Map/Mapload.jsx
@@ -99,19 +99,21 @@ export default class Mapload extends React.Component {
     map.on('draw.create', this.createArea);
     map.on('draw.delete', this.deleteArea);
     map.on('draw.update', this.updateArea);
-    // map.on('load', function () {
-    //   map.addLayer({
-    //     "id": "simple-tiles",
-    //     "type": "raster",
-    //     "source": {
-    //       "type": "raster",
-    //       "tiles": ["https://tile.openweathermap.org/map/temp_new/{z}/{x}/{y}.png?appid=874718354841f0e0250b4b06a05a971e"],
-    //       "tileSize": 256
-    //     },
-    //     "minzoom": 0,
-    //     "maxzoom": 22
-    //   });
-    // });
+    map.on('load', function () {
+      var mapCanvas = document.getElementsByClassName('mapboxgl-canvas')[0];
+      mapCanvas.style.width = '100%';
+      map.addLayer({
+        "id": "simple-tiles",
+        "type": "raster",
+        "source": {
+          "type": "raster",
+          "tiles": ["https://tile.openweathermap.org/map/temp_new/{z}/{x}/{y}.png?appid=874718354841f0e0250b4b06a05a971e"],
+          "tileSize": 256
+        },
+        "minzoom": 0,
+        "maxzoom": 22
+      });
+    });
 
   }
   switchLayer(layer) {
